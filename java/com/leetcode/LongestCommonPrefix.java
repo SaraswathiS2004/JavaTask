5668dfe9bb52d0519f186a166200220e3190eca9
SaraswathiS2004 <saraswathis1332004@gmail.com>
2025-12-02 13:18:36 +0530

    Leet Code problems

---
+++ java/com/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+public class LongestCommonPrefix {
+        public String longestCommonPrefix(String[] strs) {
+            String result = "";
+            int index = 0;
+            for(int i = 0; i< strs[0].length(); i++){
+                String s = strs[0];
+                boolean isTrue = true;
+                if(s.length() > index) {
+                    char c = s.charAt(index);
+                    int ind = 1;
+                    while (strs.length > ind) {
+                        String s1 = strs[ind];
+                        if(s1.length() <= index){
+                            return result;
+                        }
+                        char c1 = s1.charAt(index);
+                        if (c1 != c) {
+                            return result;
+                        }
+                        ind++;
+                    }
+                    if(isTrue) {
+                        result += c;
+                    }
+                    index++;
+                }
+            }
+            return result;
+        }
+}
