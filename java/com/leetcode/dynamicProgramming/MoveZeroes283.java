d2d3713eae156a9e85879476ee23e5777d76f3e9
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-02-04 10:19:48 +0530

    Leetcode

--- java/com/leetcode/dynamicProgramming/MoveZeroes283.java
+++ java/com/leetcode/dynamicProgramming/MoveZeroes283.java
@@ -3,22 +3,30 @@ package com.leetcode.dynamicProgramming;
 public class MoveZeroes283 {
     public void moveZeroes(int[] nums) {
         int index = 0;
-        for(int i = 1; i< nums.length; i++){
-            int n1 = nums[i];
-            int n2 = nums[i - 1];
-            if(n1 != 0 && n2 == 0){
-                nums[index] = n1;
-                nums[i] = n2;
-                index++;
+//        for(int i = 1; i< nums.length; i++){
+//            int n1 = nums[i];
+//            int n2 = nums[i - 1];
+//            if(n1 != 0 && n2 == 0){
+//                nums[index] = n1;
+//                nums[i] = n2;
+//                index++;
+//            }
+//            else if(n1 == 0 && n2 != 0){
+//                nums[index] = n2;
+//                nums[i] = n1;
+//                index++;
+//            }
+//            else if(n1 != 0 &&  n2 != 0){
+//                index++;
+//            }
+//        }
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] != 0){
+                nums[index++] = nums[i];
             }
-            else if(n1 == 0 && n2 != 0){
-                nums[index] = n2;
-                nums[i] = n1;
-                index++;
-            }
-            else if(n1 != 0 &&  n2 != 0){
-                index++;
-            }
+        }
+        for(int j = index; j< nums.length; j++){
+            nums[j] = 0;
         }
     }
 }
