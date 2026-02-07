eadacd1dddca3b5716c11c7a3b57343b57a924a4
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-02-07 09:24:31 +0530

    Leetcode

--- java/com/leetcode/Array/SearchInsertPosition35.java
+++ java/com/leetcode/Array/SearchInsertPosition35.java
@@ -2,12 +2,20 @@ package com.leetcode.Array;
 
 public class SearchInsertPosition35 {
     public int searchInsert(int[] nums, int target) {
-        for(int i = 0; i< nums.length; i++) {
-            int a = nums[i];
-            if (a > target || a == target) {
-                return i;
+        int start = 0;
+        int end = nums.length -1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else {
+                return mid;
             }
         }
-        return nums.length;
+        return start;
     }
 }
