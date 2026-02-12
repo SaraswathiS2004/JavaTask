8724cc30aa79584d86412b1ed02b2c77d1e43493
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-02-12 15:09:42 +0530

    Leetcode

---
+++ java/com/superEasyProblem/FindtheMaximumAchievableNumber2769.java
@@ -0,0 +1,8 @@
+package com.superEasyProblem;
+
+public class FindtheMaximumAchievableNumber2769 {
+    public int theMaximumAchievableX(int num, int t) {
+        int x = num + t * 2;
+        return x;
+    }
+}
