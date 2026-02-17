e779a93a5461231d4e5cb693d6d3be93f662bfed
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-02-17 18:25:50 +0530

    Leetcode

---
+++ java/com/leetcode/Array/SearchinRotatedSortedArray33.java
@@ -0,0 +1,34 @@
+package com.leetcode.Array;
+
+public class SearchinRotatedSortedArray33 {
+    public int search(int[] nums, int target) {
+
+        //Binary search
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[start] <= nums[mid]){
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid -1;
+                }
+                else {
+                    start = mid +1;
+                }
+            }
+            else {
+                if(target <= nums[end] && target >= nums[mid]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
