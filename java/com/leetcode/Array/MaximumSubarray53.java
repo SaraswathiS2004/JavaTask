e6a28878e1dd794129c49648b528819870ad63c7
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-02-20 15:12:13 +0530

    Leetcode

--- java/com/leetcode/Array/MaximumSubarray53.java
+++ java/com/leetcode/Array/MaximumSubarray53.java
@@ -7,17 +7,27 @@ import java.util.Map;
 
 public class MaximumSubarray53 {
 
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
 
-        public static int maxSubArray(int[] nums) {
-            int max = Integer.MIN_VALUE;
-            int sum = 0;
-            for(int i = 0; i< nums.length; i++){
-                sum += nums[i];
-                max = Math.max(max , sum);
-                if(sum < 0){
-                    sum = 0;
-                }
-            }return max;
+        //kadane's algorithm
+
+        // for(int i = 0; i< nums.length; i++){
+        //     sum += nums[i];
+        //     if(nums[i] > sum){
+        //         sum = nums[i];
+        //     }
+        //     max = Math.max(max , sum);
+
+        // }
+
+        // other solution
+        for(int i = 0; i< nums.length; i++){
+            sum = Math.max (nums[i] , sum + nums[i]);
+            max = Math.max(max , sum);
+        }
+        return max;
     }
     public static void main(String[] args){
         int [] nums ={-2,1,-3,4,-1,2,1,-5,4};
