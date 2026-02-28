ae10e5a6e08e7e84e3c44b21f26c8ff6dd85b31f
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-02-28 15:52:17 +0530

    Leetcode

---
+++ java/com/leetcode/Array/FindMinimuminRotatedSortedArrayII154.java
@@ -0,0 +1,22 @@
+package com.leetcode.Array;
+
+public class FindMinimuminRotatedSortedArrayII154 {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length -1;
+        while(left < right){
+            int mid = (left + right) /2;
+            if(nums[mid] > nums[right]){
+                left = mid +1;
+            }
+            else if(nums[mid] > nums[left]){
+                right = mid;
+            }
+            else{
+                right --;
+            }
+
+        }
+        return nums[left];
+    }
+}
