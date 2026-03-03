9f8de8a5b76a8d9fd7240afdc12da7ac510a3a76
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-03-03 22:45:24 +0530

    Leetcode

---
+++ java/com/leetcode/dp_programs/UniquePaths62.java
@@ -0,0 +1,21 @@
+package com.leetcode.dp_programs;
+
+import java.util.Arrays;
+
+public class UniquePaths62 {
+    public int uniquePaths(int m, int n) {
+        int [][] dp = new int[m][n];
+        int num = findUniquePaths(m ,n , 0 , 0 , dp);
+        return num;
+    }
+    public static int findUniquePaths(int m, int n , int x ,int y , int [][] dp){
+        if(x >= m || y >= n)return 0;
+        else if( x == m -1 && y == n -1) return 1;
+        else if(dp[x][y] != 0) return dp[x][y];
+
+        int right = findUniquePaths(m , n , x , y+1 , dp);
+        int left = findUniquePaths(m , n , x+1 , y , dp);
+        dp[x][y] = right + left;
+        return dp[x][y];
+    }
+}
