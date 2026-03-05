65974e08026853d99e04642fb10a329ca645042e
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-03-05 21:49:11 +0530

    Leetcode

--- java/com/leetcode/Array/TopInterview/ProductofArrayExceptSelf.java
+++ java/com/leetcode/Array/TopInterview/ProductofArrayExceptSelf.java
@@ -4,26 +4,26 @@ import java.util.Arrays;
 
 public class ProductofArrayExceptSelf {
     public static int[] productExceptSelf(int[] nums) {
-//    //    int [] prefix = new int[nums.length];
-//    //     int [] suffix = new int[nums.length];
-//    //     int start = nums[0];
-//    //     //find prefix
-//    //     prefix[0] = 1;
-//    //     suffix[nums.length -1] = 1;
-//    //     for(int i = 1; i< nums.length; i++){
-//    //         prefix[i] = nums[i -1] * prefix[i -1];
-//    //     }
-//    //     //find suffix
-//    //     for(int k = nums.length - 1; k>0; k--){
-//    //         suffix[k -1] = nums[k] * suffix[k];
-//    //     }
-//    //     for(int m = 0; m< prefix.length; m++){
-//    //         suffix[m] = suffix[m] * prefix[m];
-//    //     }
-//    //     return suffix;
-//
-//    // Another method
-//
+    //    int [] prefix = new int[nums.length];
+   //     int [] suffix = new int[nums.length];
+    //     int start = nums[0];
+    //     //find prefix
+    //     prefix[0] = 1;
+    //     suffix[nums.length -1] = 1;
+    //     for(int i = 1; i< nums.length; i++){
+    //         prefix[i] = nums[i -1] * prefix[i -1];
+    //     }
+    //     //find suffix
+    //     for(int k = nums.length - 1; k>0; k--){
+    //         suffix[k -1] = nums[k] * suffix[k];
+    //     }
+    //     for(int m = 0; m< prefix.length; m++){
+    //         suffix[m] = suffix[m] * prefix[m];
+    //     }
+    //     return suffix;
+
+   // Another method
+
     int [] result = new int[nums.length];
 
     result [0] = 1;
